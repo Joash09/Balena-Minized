48daf805776e142bdbd3e8278c4a8fed1df49a85
Joash09 <NDXJOA004@myuct.ac.za>
2019-08-26 23:24:20 +0200

    Working build script. Added meta-balena-minized layer

--- build.sh
+++ build.sh
@@ -2,13 +2,15 @@
 
 echo "Begin build BalenaOS for Xilinx Minized board"
 
+# export MACHINE="minized-zynq7"
+export TEMPLATECONF="../meta-balena-minized/conf/samples/"
 source ./layers/poky/oe-init-build-env build
 
 # This could all be replaced with a sample bblayes.conf file
-bitbake-layers add-layer ../layers/meta-openembedded/meta-oe/
-bitbake-layers add-layer ../layers/meta-openembedded/meta-python/
-bitbake-layers add-layer ../layers/meta-openembedded/meta-perl/
-bitbake-layers add-layer ../layers/meta-xilinx/meta-xilinx-bsp/
-bitbake-layers add-layer ../layers/meta-xilinx/meta-xilinx-contrib/
+# bitbake-layers add-layer ../layers/meta-openembedded/meta-oe/
+# bitbake-layers add-layer ../layers/meta-openembedded/meta-python/
+# bitbake-layers add-layer ../layers/meta-openembedded/meta-perl/
+# bitbake-layers add-layer ../layers/meta-xilinx/meta-xilinx-bsp/
+# bitbake-layers add-layer ../layers/meta-xilinx/meta-xilinx-contrib/
 
 bitbake core-image-minimal
