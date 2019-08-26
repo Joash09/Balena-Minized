1aa8b2e007bca7c6dbf317cba15bc3c828d1dc26
Joash09 <ndxjoa004@myuct.ac.za>
2019-08-26 16:46:41 +0200

    Playing around with build script

--- build.sh
+++ build.sh
@@ -3,3 +3,12 @@
 echo "Begin build BalenaOS for Xilinx Minized board"
 
 source ./layers/poky/oe-init-build-env build
+
+# This could all be replaced with a sample bblayes.conf file
+bitbake-layers add-layer ../layers/meta-openembedded/meta-oe/
+bitbake-layers add-layer ../layers/meta-openembedded/meta-python/
+bitbake-layers add-layer ../layers/meta-openembedded/meta-perl/
+bitbake-layers add-layer ../layers/meta-xilinx/meta-xilinx-bsp/
+bitbake-layers add-layer ../layers/meta-xilinx/meta-xilinx-contrib/
+
+bitbake core-image-minimal
