3ba4ed8bfab96f25073f37eaff3645c3607d7cec
Joash09 <NDXJOA004@myuct.ac.za>
2019-09-24 07:51:48 +0200

    Finalizing new branch

--- build.sh
+++ build.sh
@@ -2,4 +2,15 @@
 
 echo "Begin build BalenaOS for Xilinx Minized board"
 
-source poky/oe-init-build-env
+# export MACHINE="minized-zynq7"
+export TEMPLATECONF="../layers/meta-balena-minized/conf/samples/"
+source ./layers/poky/oe-init-build-env build-xlnx
+
+# This could all be replaced with a sample bblayes.conf file
+# bitbake-layers add-layer ../layers/meta-openembedded/meta-oe/
+# bitbake-layers add-layer ../layers/meta-openembedded/meta-python/
+# bitbake-layers add-layer ../layers/meta-openembedded/meta-perl/
+# bitbake-layers add-layer ../layers/meta-xilinx/meta-xilinx-bsp/
+# bitbake-layers add-layer ../layers/meta-xilinx/meta-xilinx-contrib/
+
+bitbake core-image-minimal
